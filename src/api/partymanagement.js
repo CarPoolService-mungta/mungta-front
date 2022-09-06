88e7e463848300258e9ba31368d3a3b8a1867427
honeion <honeion2012@gmail.com>
2022-09-06 22:10:41 +0900

    controller path 맞춰서 수정

--- src/api/partymanagement.js
+++ src/api/partymanagement.js
@@ -10,21 +10,19 @@ import stringify from 'json-stringify';
  * @returns
  */
 export const getPartyInfoAllNow = async params =>
-       axiosInstance.get('/partyInfos/carpool-now-list',{params});
+       axiosInstance.get('/party-management/partyInfos/carpool-now-list',{params});
 
-// export const postMoveInfo = async (data) =>
-//        axiosInstance.post('/partyInfos/post-moveinfo',data);
-// export const postMoveInfo = async (data)=>{
-//     console.log('postMoveInfo다',data);
-//     return axios.post(process.env.REACT_APP_API_SERVER+'/api/partyInfos/post-moveinfo',data);
-// }
+/**
 
+
+
+ */
 export const postMoveInfo = async function (Data) {
     console.log('이거 넣는다',Data)
     console.log('이거 넣는다',Data.curNumberOfParty)
     console.log('이거 넣는다',Data.maxNumberOfParty)
     console.log('이거 넣는다2',stringify(Data))
-    let url =process.env.REACT_APP_API_SERVER + '/api/partyInfos/post-moveinfo';
+    let url =process.env.REACT_APP_API_SERVER + '/api/party-managementpartyInfos/post-moveinfo';
     let headers ={  "Content-Type": 'application/json',}
     //formdata로 해야 읽히는디.. 안되는디 json으로 읽어야함 객체안에 객체있어서
     console.log('url:',url);
@@ -35,18 +33,10 @@ export const postMoveInfo = async function (Data) {
     return response;
 }
 
-
 export const postMoveInfo2 = async function (Data) {
-
-    const response = await axios.post(process.env.REACT_APP_API_SERVER + '/api/partyInfos/post-moveinfo2', stringify(Data));
+    const response = await axios.post(process.env.REACT_APP_API_SERVER + '/api/party-managementpartyInfos/post-moveinfo2', stringify(Data));
     return response.data;
 }
-/**
- * 현재 참여 가능한 카풀리스트 전체 검색
- * condition : "start_date", "distance", "review_average_score", ""
- * @param {*} params
- * @returns
- */
-export const postMoveInfo3 = async params =>
-        axiosInstance.post('/partyInfos/post-moveinfo',{params});
+export const postMoveInfo3 = async (params) =>
+        axiosInstance.post('/party-management/partyInfos/post-moveinfo',params);
 
