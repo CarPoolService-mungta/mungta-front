cfe7edf0f1bcb40c20e39a3923ccb0c76002d3d5
Julia <julberry09@gmail.com>
2022-09-14 03:41:19 +0900

    user login

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,4 @@
 import {axios} from 'utils';
 
-// 사용자 로그인
-export const signinById = async data => {
-  return axios.post('/api/user/auth/signin', data);
-}
+export const signinById = async data => axios.post('/api/user/auth/signin', data);
+
