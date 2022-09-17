cff7d313b7a3931d50b15adc4a7b698dc655b394
ljj9885 <ljj9885@gmail.com>
2022-09-17 09:16:48 +0900

    update

--- src/api/partyMatching.js
+++ src/api/partyMatching.js
@@ -3,11 +3,13 @@ import { axios } from 'utils';
 /**
  * @param {{partyInfoId: number, userId: number}} params
  */
-export const getMatchInfo = async (params) => axios.get('/match/matchInfo', { params });
+export const getMatchInfo = async (params) =>
+  axios.get('/match/matchInfo', { params });
 /**
  * @param {{partyInfoId: number, userId: number}} params
  */
-export const getMatchUsers = async (params) => axios.get('/match/partymembers', { params });
+export const getMatchUsers = async (params) =>
+  axios.get('/match/partymembers', { params });
 /**
  * @param {{partyInfoId: number, userId: number}} params
  */
@@ -15,8 +17,25 @@ export const applyParty = async (data) => axios.post('/match/apply', data);
 /**
  * @param {{userId: number}} params
  */
-export const cancelParty = async (data) => axios.post('/match/cancel', data);
+export const cancelMatch = async (data) => axios.post('/match/cancel', data);
 /**
  * @param {{partyId: number}} params
  */
-// export const getPartyInfo = async (data) => axios.post('/matches/getMatchInfo', data);
+export const acceptMatch = async (data) => axios.post('/match/accept', data);
+/**
+ * @param {{partyId: number}} params
+ */
+export const denyMatch = async (data) => axios.post('/match/deny', data);
+/**
+ * @param {{partyId: number}} params
+ */
+export const startParty = async (matchProcess) => axios.post('/match/partyStart', matchProcess);
+/**
+ * @param {{partyId: number}} params
+ */
+export const closeParty = async (matchProcess) => axios.post('/match/partyClose', matchProcess);
+/**
+ * @param {{partyId: number}} params
+ */
+// export const cancelParty = async (data) =>
+//   axios.post('/match/partyCancel', data);
--- src/pages/party-matching/PartyMemberList.js
+++ src/pages/party-matching/PartyMemberList.js
@@ -1,21 +1,23 @@
 import PropTypes from 'prop-types';
 import { useCallback, useEffect, useState } from 'react';
+import { SnackbarProvider } from 'notistack';
 import { getMatchUsers } from 'api/partyMatching';
 import UserTable from 'pages/party-matching/UserTable';
+import WaitTable from 'pages/party-matching/WaitTable';
 import DataTable from 'components/@extended/DataTable';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
 import {
-    Avatar,
-    Grid,
-    List,
-    ListItemAvatar,
-    ListItemButton,
-    ListItemSecondaryAction,
-    ListItemText,
-    Stack,
-    Typography
+  Avatar,
+  Grid,
+  List,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemSecondaryAction,
+  ListItemText,
+  Stack,
+  Typography,
 } from '@mui/material';
 
 // project import
@@ -23,122 +25,143 @@ import MainCard from 'components/MainCard';
 import ComponentSkeleton from 'pages/components-overview/ComponentSkeleton';
 
 // assets
-import { GiftOutlined, MessageOutlined, SettingOutlined } from '@ant-design/icons';
+import {
+  GiftOutlined,
+  MessageOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
 import { TableRow } from '../../../node_modules/@mui/material/index';
 
 // avatar style
 const avatarSX = {
-    width: 36,
-    height: 36,
-    fontSize: '1rem'
+  width: 36,
+  height: 36,
+  fontSize: '1rem',
 };
 
 // action style
 const actionSX = {
-    mt: 0.75,
-    ml: 1,
-    top: 'auto',
-    right: 'auto',
-    alignSelf: 'flex-start',
-    transform: 'none'
+  mt: 0.75,
+  ml: 1,
+  top: 'auto',
+  right: 'auto',
+  alignSelf: 'flex-start',
+  transform: 'none',
 };
 
 const columns = [
-    {
-        id: 'userId',
-        label: '사용자명',
-        width: 290,
-        align: 'left'
-    }
+  {
+    id: 'userId',
+    label: '사용자명',
+    width: 290,
+    align: 'left',
+  },
 ];
 
+function isDriver() {
+    return true;
+}
+
 const PartyMemberList = () => {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const [matchResult, setMatchResult] = useState(null);
+  const [userResult, setUserResult] = useState(null);
+  const [waitResult, setWaitResult] = useState(null);
 
-    const nameList =
-        matchResult &&
-        matchResult.map((user) => (
-            <UserTable
-                userId={user.userId}
-                curPhoto={user.curPhoto}
-                userTeamName={user.userTeamName}
-                content={user.content}
-                reviewScore={user.reviewScore}
-            />
-        ));
-    const acceptList =
-        matchResult &&
-        matchResult.map((user) => (
-            <UserTable
-                userId={user.userId}
-                curPhoto={user.curPhoto}
-                userTeamName={user.userTeamName}
-                content={user.content}
-                reviewScore={user.reviewScore}
-            />
-        ));
+  const acceptList =
+    userResult &&
+    userResult.map((user) => (
+      <UserTable
+        userId={user.userId}
+        curPhoto={user.curPhoto}
+        userTeamName={user.userTeamName}
+        content={user.content}
+        reviewScore={user.reviewScore}
+      />
+    ));
+  const nameList =
+    waitResult &&
+    waitResult.map((user) => (
+      <WaitTable
+        userId={user.userId}
+        curPhoto={user.curPhoto}
+        userTeamName={user.userTeamName}
+        content={user.content}
+        reviewScore={user.reviewScore}
+      />
+    ));
 
-    useEffect(async () => {
-        const partyInfoId = 2;
-        const matchStatus = 'WAITING';
-        const matchResult = await getMatchUsers({ partyInfoId, matchStatus });
-        setMatchResult(matchResult);
-        console.log(matchResult);
-    }, []);
+  useEffect(async () => {
+    const partyInfoId = 2;
+    const matchStatus = 'WAITING';
+    const matchResult = await getMatchUsers({ partyInfoId, matchStatus });
+    setWaitResult(matchResult);
+  }, []);
 
-    return (
-        <ComponentSkeleton>
-            <Grid container spacing={3}>
-                <Grid item sx={{ mt: 2 }}>
-                    <Typography variant="h5">파티 확정 멤버</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <MainCard content={false}>
-                        <List
-                            component="nav"
-                            sx={{
-                                px: 0,
-                                py: 0,
-                                '& .MuiListItemButton-root': {
-                                    py: 1.5,
-                                    '& .MuiAvatar-root': avatarSX,
-                                    '& .MuiListItemSecondaryAction-root': { ...actionSX, position: 'relative' }
-                                }
-                            }}
-                        >
-                            {acceptList}
-                        </List>
-                    </MainCard>
-                </Grid>
-            </Grid>
+  useEffect(async () => {
+    const partyInfoId = 2;
+    const matchStatus = 'ACCEPT';
+    const matchResult = await getMatchUsers({ partyInfoId, matchStatus });
+    setUserResult(matchResult);
+  }, []);
 
-            <Grid container spacing={3} sx={{ mt: 2 }}>
-                <Grid item sx={{ mt: 2 }}>
-                    <Typography variant="h5">파티 요청 멤버</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <MainCard content={false}>
-                        <List
-                            component="nav"
-                            sx={{
-                                px: 0,
-                                py: 0,
-                                '& .MuiListItemButton-root': {
-                                    py: 1.5,
-                                    '& .MuiAvatar-root': avatarSX,
-                                    '& .MuiListItemSecondaryAction-root': { ...actionSX, position: 'relative' }
-                                }
-                            }}
-                        >
-                            {nameList}
-                        </List>
-                    </MainCard>
-                </Grid>
-            </Grid>
-        </ComponentSkeleton>
-    );
+  return (
+    <ComponentSkeleton>
+      <Grid container spacing={3}>
+        <Grid item sx={{ mt: 2 }}>
+          <Typography variant="h5">파티 확정 멤버</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <MainCard content={false}>
+            <List
+              component="nav"
+              sx={{
+                px: 0,
+                py: 0,
+                '& .MuiListItemButton-root': {
+                  py: 1.5,
+                  '& .MuiAvatar-root': avatarSX,
+                  '& .MuiListItemSecondaryAction-root': {
+                    ...actionSX,
+                    position: 'relative',
+                  },
+                },
+              }}
+            >
+              {acceptList}
+            </List>
+          </MainCard>
+        </Grid>
+      </Grid>
+
+      {isDriver() && <Grid container spacing={3} sx={{ mt: 2 }}>
+        <Grid item sx={{ mt: 2 }}>
+          <Typography variant="h5">파티 요청 멤버</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <MainCard content={false}>
+            <List
+              component="nav"
+              sx={{
+                px: 0,
+                py: 0,
+                '& .MuiListItemButton-root': {
+                  py: 1.5,
+                  '& .MuiAvatar-root': avatarSX,
+                  '& .MuiListItemSecondaryAction-root': {
+                    ...actionSX,
+                    position: 'relative',
+                  },
+                },
+              }}
+            >
+              {nameList}
+            </List>
+          </MainCard>
+        </Grid>
+      </Grid>}
+    </ComponentSkeleton>
+  );
 };
 
 export default PartyMemberList;
